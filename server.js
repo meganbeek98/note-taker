eab17c07d6ee62bcd3209a06370c98b59699b8aa
meganbeek98 <meganbeek98@gmail.com>
2022-07-01 04:30:37 -0400

    notes.js

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes= require('./routes/htmlRoutes');
