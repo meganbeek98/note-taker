76077056891fb51865e9f3431b6f33cd5aeeecb9
meganbeek98 <meganbeek98@gmail.com>
2022-07-01 05:28:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes= require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
