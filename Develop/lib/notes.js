55f9bcc8c6f5c047686877aebab394d0afedb442
meganbeek98 <meganbeek98@gmail.com>
2022-07-01 06:08:12 -0400

    createNewNote

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -18,4 +18,12 @@ function updateDb(id, notesArray) {
     }
 }
 
-function createNewNote(body, notesArray) {}
+function createNewNote(body, notesArray) {
+    const newNote = body
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, "../db/db.json"),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return newNote;
+}
